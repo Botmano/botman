b5120899f54e831e4fde3fb22f2ad1a5062eb23c
Botmano <70124373+Botmano@users.noreply.github.com>
2020-10-09 22:55:19 -0300

    Add files via upload

---
+++ 17- Linha de Comando/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+	"linha-de-comando/app"
+)
+
+
+func main() {
+
+	aplicacao := app.Gerar()
+	if erro := aplicacao.Run(os.Args); erro != nil {
+		log.Fatal(erro)
+	} //Reconhecimento terminal
+	
+	
+}
